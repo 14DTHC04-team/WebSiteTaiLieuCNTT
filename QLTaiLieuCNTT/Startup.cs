24de1fe9373ecc9af2f14b216fa22fc0d8f004ae
HongDang <hongdang2691996@gmail.com>
2017-10-05 11:29:11 +0700

    Add project files.

---
+++ QLTaiLieuCNTT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QLTaiLieuCNTT.Startup))]
+namespace QLTaiLieuCNTT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
